9676a6f5798dd27fd105b2d1c46e475ae20ef561
PaavanShah <paavanrshah@vadict.com>
2023-05-09 12:42:14 +0530

    removing manual creation of hello service bean

--- hello-app/src/main/java/org/example/HelloApp.java
+++ hello-app/src/main/java/org/example/HelloApp.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import hello.ConsoleHelloService;
-import hello.HelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @SpringBootApplication is meta annotated with three different annotations: @SpringBootConfiguration, @ComponentScan, @EnableAutoConfiguration
@@ -14,9 +11,5 @@ import org.springframework.context.annotation.Bean;
 public class HelloApp {
     public static void main(String[] args) {
         SpringApplication.run(HelloApp.class, args);
-    }
-    @Bean
-    public HelloService helloService(){
-        return new ConsoleHelloService();
     }
 }
