f2775019a6d91c642e2920efc0b27cad6216a47e
PaavanShah <paavanrshah@vadict.com>
2023-05-09 12:04:02 +0530

    creating hello-service and using the service in hello-app project.

---
+++ hello-app/src/main/java/org/example/HelloApp.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import hello.ConsoleHelloService;
+import hello.HelloService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @SpringBootApplication is meta annotated with three different annotations: @SpringBootConfiguration, @ComponentScan, @EnableAutoConfiguration
+ * @ComponentScan: If any class is annotated with @Component annotation it will be picked up in component scan. This will register all the beans that user has specified and then autoconfiguration runs.
+ * @EnableAutoConfiguration: This will add additional beans that are required to run the dependencies we have added*/
+@SpringBootApplication
+public class HelloApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApp.class, args);
+    }
+    @Bean
+    public HelloService helloService(){
+        return new ConsoleHelloService();
+    }
+}
---
+++ hello-app/src/main/java/org/example/HelloCommandLineRunner.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import hello.HelloService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloCommandLineRunner implements CommandLineRunner {
+    private final HelloService service;
+    /**
+     * The constructor needs a parameter of type HelloService. One way to that is instantiate the class using this
+     * constructor or make a bean of HelloService. If there is a bean of type HelloService it will automatically
+     * take it and use it to instantiate the HelloCommandLineRunner class.*/
+    public HelloCommandLineRunner(HelloService service){
+        this.service = service;
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        this.service.sayHello("World");
+    }
+}
